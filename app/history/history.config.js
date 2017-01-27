d65e2fb4bada602e8bce85c90a0dbc3041e97005
Joey Resuento <Joey.Resuento@sizmek.com>
2017-01-27 20:21:49 +0800

    History config code changes

--- app/history/history.config.js
+++ app/history/history.config.js
@@ -13,52 +13,7 @@ component('historyView', {
 			self.isShowLogDetails = false;
 			self.selectedHistoryID = null;
 			self.selectedHistoryDetails = {};
-			self.entityHistoryResult = new Array;
-
-			//Testing
-			self.authorization = 'd90b4ca7-f1fa-48de-8f27-0eaf12cfc6df', self.entityType = 'Ad', self.entityID = '1074048485'
-			//endTesting
-
-			self.showLogDetails = function(){
-				angular.forEach(self.entityHistoryResult, function(value) {
-					if(value.id == self.selectedHistoryID) self.selectedHistoryDetails = value;
-				});
-
-				self.isShowLogDetails = true;
-				self.property_change_table = '<div class=\'row property-change-table-row-header\'>';
-				self.property_change_table += '<div class=\'col-md-3\'>ParentField</div>';
-				self.property_change_table += '<div class=\'col-md-3\'>Field</div>';
-				self.property_change_table += '<div class=\'col-md-3\'>New Value</div>';
-				self.property_change_table += '<div class=\'col-md-3\'>Old Value</div>';
-				self.property_change_table += '</div>';
-
-				if(typeof self.selectedHistoryDetails.originJsonValue.propertyChange === 'undefined'){
-					self.property_change_table += '<div class=\'row\'>';
-					self.property_change_table += '<div class=\'col-md-12\'>No property changes to show.</div>';
-					self.property_change_table += '</div>';
-				} else angular.forEach(self.selectedHistoryDetails.originJsonValue.propertyChange.propertyChanges, function(v) {
-					
-					if(typeof v.propertyChanges != 'object'){
-						self.property_change_table += '<div class=\'row\'>';
-						self.property_change_table += '<div title=\'' + v.parentField + '\' class=\'col-md-3\'>' + v.parentField + '</div>';
-						self.property_change_table += '<div title=\'' + v.field + '\' class=\'col-md-3\'>' + v.field + '</div>';
-						self.property_change_table += '<div title=\'' + v.valueNew + '\' class=\'col-md-3\'>' + v.valueNew + '</div>';
-						self.property_change_table += '<div title=\'' + v.valueOld + '\' class=\'col-md-3\'>' + v.valueOld + '</div>';
-						self.property_change_table += '</div>';
-					} else {
-						angular.forEach(v.propertyChanges, function(vc) {							
-							self.property_change_table += '<div class=\'row\'>';
-							self.property_change_table += '<div title=\'' + vc.parentField + '\' class=\'col-md-3\'>' + vc.parentField + '</div>';
-							self.property_change_table += '<div title=\'' + vc.field + '\' class=\'col-md-3\'>' + vc.field + '</div>';
-							self.property_change_table += '<div title=\'' + vc.valueNew + '\' class=\'col-md-3\'>' + vc.valueNew + '</div>';
-							self.property_change_table += '<div title=\'' + vc.valueOld + '\' class=\'col-md-3\'>' + vc.valueOld + '</div>';
-							self.property_change_table += '</div>';
-						});
-					}
-
-				});
-				self.highlightSelectedRow();
-			}
+			self.entityHistoryResult = new Array;				
 
 			self.hideLogDetails = function(){
 				self.isShowLogDetails = false;	
@@ -105,6 +60,51 @@ component('historyView', {
 					(historyTableViewDivs[i].className.indexOf('rowselected') > -1) && (historyTableViewDivs[i].className = historyTableViewDivs[i].className.replace('rowselected', ''))
 				}
 			}
+
+			self.showLogDetails = function(){
+				angular.forEach(self.entityHistoryResult, function(value) {
+					if(value.id == self.selectedHistoryID) self.selectedHistoryDetails = value;
+				});
+
+				self.isShowLogDetails = true;
+				self.property_change_table = '<div class=\'row property-change-table-row-header\'>';
+				self.property_change_table += '<div class=\'col-md-3\'>ParentField</div>';
+				self.property_change_table += '<div class=\'col-md-3\'>Field</div>';
+				self.property_change_table += '<div class=\'col-md-3\'>New Value</div>';
+				self.property_change_table += '<div class=\'col-md-3\'>Old Value</div>';
+				self.property_change_table += '</div>';
+
+				if(typeof self.selectedHistoryDetails.originJsonValue.propertyChange === 'undefined'){
+					self.property_change_table += '<div class=\'row\'>';
+					self.property_change_table += '<div class=\'col-md-12\'>No property changes to show.</div>';
+					self.property_change_table += '</div>';
+				} else angular.forEach(self.selectedHistoryDetails.originJsonValue.propertyChange.propertyChanges, function(v) {
+					
+					if(typeof v.propertyChanges != 'object'){
+						self.property_change_table += '<div class=\'row\'>';
+						self.property_change_table += '<div title=\'' + v.parentField + '\' class=\'col-md-3\'>' + v.parentField + '</div>';
+						self.property_change_table += '<div title=\'' + v.field + '\' class=\'col-md-3\'>' + v.field + '</div>';
+						self.property_change_table += '<div title=\'' + v.valueNew + '\' class=\'col-md-3\'>' + v.valueNew + '</div>';
+						self.property_change_table += '<div title=\'' + v.valueOld + '\' class=\'col-md-3\'>' + v.valueOld + '</div>';
+						self.property_change_table += '</div>';
+					} else {
+						angular.forEach(v.propertyChanges, function(vc) {							
+							self.property_change_table += '<div class=\'row\'>';
+							self.property_change_table += '<div title=\'' + vc.parentField + '\' class=\'col-md-3\'>' + vc.parentField + '</div>';
+							self.property_change_table += '<div title=\'' + vc.field + '\' class=\'col-md-3\'>' + vc.field + '</div>';
+							self.property_change_table += '<div title=\'' + vc.valueNew + '\' class=\'col-md-3\'>' + vc.valueNew + '</div>';
+							self.property_change_table += '<div title=\'' + vc.valueOld + '\' class=\'col-md-3\'>' + vc.valueOld + '</div>';
+							self.property_change_table += '</div>';
+						});
+					}
+
+				});
+				self.highlightSelectedRow();
+			}
+
+			//Testing
+			self.authorization = 'd90b4ca7-f1fa-48de-8f27-0eaf12cfc6df', self.entityType = 'Ad', self.entityID = '1074048485'
+			//endTesting		
 		}
 	]
 });
